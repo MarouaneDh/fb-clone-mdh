ebdba5888e4f2ea8e93fe165bd10e24aab122cbf
marouane <dhambrimarouane@gmail.com>
2021-05-13 23:10:08 +0100

    first commit

--- index.js
+++ index.js
@@ -52,3 +52,12 @@ app.use("/api/posts", postRoute);
 app.listen(8800, () => {
   console.log("Backend server is running!");
 });
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
