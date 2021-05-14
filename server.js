a339fc1243c16a963341c77707bc0070b5ff83af
marouane <dhambrimarouane@gmail.com>
2021-05-14 22:55:45 +0100

    commited from server folder

--- server.js
+++ server.js
@@ -53,6 +53,15 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
+app.get("/", (req, res) => {
+  res
+    .set(
+      "Content-Security-Policy",
+      "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'"
+    )
+    .send("<html><head></head><body></body></html>");
+});
+
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
